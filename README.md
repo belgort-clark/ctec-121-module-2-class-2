393154fe32e476c2205363b000337403c6475465
Bruce Elgort <bruce.elgort@gmail.com>
2022-01-13 06:45:13 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ctec-121-module-2-class-2
+
+This repository will be used in class on Thursday, January 13, 2022
---
+++ thursday_1.py
@@ -0,0 +1,12 @@
+# thursday_1.py
+
+"""
+CTEC 121
+Date: 01/14/2022
+YOUR NAME
+"""
+
+def main():
+    pass # Your code goes here. This line should be removed
+
+main()
